643a971a9fd4229f53568c63bfeff92f8002c5fa
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2025-01-17 20:35:36 +0700

    feat: add entities

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,1 +1,2 @@
 DROP TABLE orders;
+DROP TABLE clients;
--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,3 +1,13 @@
+CREATE TABLE clients (
+    id UUID PRIMARY KEY,
+    email VARCHAR(128) NOT NULL
+);
+
 CREATE TABLE orders (
-    id UUID PRIMARY KEY
+    id UUID PRIMARY KEY,
+    client_id UUID REFERENCES clients(id),
+    created_at TIMESTAMP NOT NULL,
+    confirmed_at TIMESTAMP,
+    sended_at TIMESTAMP,
+    delivered_at TIMESTAMP
 );
