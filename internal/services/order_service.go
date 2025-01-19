52632534b782a0882daeacedf74adc829d0b65d7
Yesenia Sunshine <yesenias@ex-d6.nsk.21-school.ru>
2025-01-19 21:35:16 +0700

    send order instead string to queues

--- internal/services/order_service.go
+++ internal/services/order_service.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"log/slog"
 
+	"github.com/google/uuid"
 	"github.com/rozhnof/order-service/internal/models"
 )
 
 type OrderRepository interface {
 	Create(ctx context.Context, order models.Order) (models.Order, error)
+	GetByID(ctx context.Context, orderID uuid.UUID) (models.Order, error)
+	Update(ctx context.Context, order models.Order) error
+	Delete(ctx context.Context, order models.Order) error
 }
 
 type OrderService struct {
@@ -40,11 +44,17 @@ func (s OrderService) CreateOrder(ctx context.Context, clientEmail string) (mode
 		return models.Order{}, err
 	}
 
-	if err := s.createdOrderSender.SendMessage(ctx, "CreatedOrderMessage"); err != nil {
+	createdOrderMessage := CreatedOrderMessage{
+		Order: order,
+	}
+	if err := s.createdOrderSender.SendMessage(ctx, createdOrderMessage); err != nil {
 		slog.Warn("failed send CreatedOrderMessage", slog.String("error", err.Error()))
 	}
 
-	if err := s.processedOrderSender.SendMessage(ctx, "ProcessedOrderMessage"); err != nil {
+	processedOrderMessage := ProcessedOrderMessage{
+		Order: order,
+	}
+	if err := s.processedOrderSender.SendMessage(ctx, processedOrderMessage); err != nil {
 		slog.Warn("failed send ProcessedOrderMessage", slog.String("error", err.Error()))
 	}
 
