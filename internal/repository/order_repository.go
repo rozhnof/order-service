73d70d8f59c4f553444e60266a56eda0b4cb057e
Yesenia Sunshine <yesenias@ex-d6.nsk.21-school.ru>
2025-01-19 21:34:04 +0700

    feat: add order repo

--- internal/repository/order_repository.go
+++ internal/repository/order_repository.go
@@ -2,8 +2,9 @@ package repository
 
 import (
 	"context"
-	"errors"
 
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
 	"github.com/rozhnof/order-service/internal/models"
 	"github.com/rozhnof/order-service/internal/pkg/postgres"
 )
@@ -24,11 +25,58 @@ func NewOrderRepository(db postgres.Database) OrderRepository {
 }
 
 func (r OrderRepository) Create(ctx context.Context, order models.Order) (models.Order, error) {
-	const query = `INSERT INTO orders(id) VALUES ($1)`
+	var clientID uuid.UUID
+	if err := r.db.QueryRow(ctx, createClientQuery, order.ClientEmail).Scan(&clientID); err != nil {
+		return models.Order{}, err
+	}
 
-	if _, err := r.db.Exec(ctx, query, order.ID); err != nil {
+	if err := r.db.QueryRow(ctx, createOrderQuery, clientID).Scan(&order.ID); err != nil {
 		return models.Order{}, err
 	}
 
 	return order, nil
+}
+
+func (r OrderRepository) GetByID(ctx context.Context, orderID uuid.UUID) (models.Order, error) {
+	var (
+		order    models.Order
+		clientID uuid.UUID
+	)
+	if err := r.db.QueryRow(ctx, getOrderQuery, orderID).Scan(
+		&clientID,
+		&order.CreatedAt,
+		&order.ConfirmedAt,
+		&order.SendedAt,
+		&order.DeliveredAt,
+	); err != nil {
+		return models.Order{}, err
+	}
+
+	var clientEmail string
+	if err := r.db.QueryRow(ctx, getClientQuery, clientID).Scan(&clientEmail); err != nil {
+		return models.Order{}, err
+	}
+
+	return order, nil
+}
+
+func (r OrderRepository) Update(ctx context.Context, order models.Order) error {
+	var clientID uuid.UUID
+	if err := r.db.QueryRow(ctx, createClientQuery, order.ClientEmail).Scan(&clientID); err != nil {
+		return err
+	}
+
+	if _, err := r.db.Exec(ctx, updateOrderQuery, clientID, order.ConfirmedAt, order.SendedAt, order.DeliveredAt); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r OrderRepository) Delete(ctx context.Context, order models.Order) error {
+	if _, err := r.db.Exec(ctx, deleteOrderQuery, order.ID); err != nil {
+		return err
+	}
+
+	return nil
 }
