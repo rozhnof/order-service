6d8ecc920870f23884bf50cdc85c7fed21bce536
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2025-01-20 13:25:35 +0700

    add scripts dir

---
+++ scripts/create_local_env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ENV_FILE_PATH=".env"
+
+ENV_VARS=(
+    "POSTGRES_ADDRESS=localhost"
+    "POSTGRES_PORT=5432"
+    "POSTGRES_USER=user"
+    "POSTGRES_PASSWORD=password"
+    "POSTGRES_DB=order_db"
+
+    "RABBITMQ_ADDRESS=localhost"
+    "RABBITMQ_PORT=5672"
+    "RABBITMQ_DEFAULT_USER=user"
+    "RABBITMQ_DEFAULT_PASS=password"
+
+    "CONFIG_PATH=config/config.yaml"
+
+    "MAIL_EMAIL=golang.auth.service@gmail.com"
+    "MAIL_PASSWORD=jybh ayjb qosq kykn"
+    "MAIL_ADDRESS=smtp.gmail.com"
+    "MAIL_PORT=587"
+)
+
+echo "" > $ENV_FILE_PATH
+
+for VAR in "${!ENV_VARS[@]}"; do
+  echo "${ENV_VARS[$VAR]}" >> $ENV_FILE_PATH
+done
