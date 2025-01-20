5775824f25446288c7bce32e9f13106853cec434
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2025-01-20 12:02:17 +0700

    change port type from int to string

--- internal/pkg/config/rabbitmq_config.go
+++ internal/pkg/config/rabbitmq_config.go
@@ -2,7 +2,7 @@ package config
 
 type RabbitMQ struct {
 	Address  string `env:"RABBITMQ_ADDRESS"          env-required:"true"`
-	Port     int    `env:"RABBITMQ_PORT"             env-required:"true"`
+	Port     string `env:"RABBITMQ_PORT"             env-required:"true"`
 	User     string `env:"RABBITMQ_DEFAULT_USER"     env-required:"true"`
 	Password string `env:"RABBITMQ_DEFAULT_PASSWORD" env-required:"true"`
 }
