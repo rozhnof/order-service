0d649e41f6d9d3ef04dc552353b84db547fe1cb1
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2025-01-20 13:23:35 +0700

    feat: add default values

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE clients (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR(128) NOT NULL UNIQUE
 );
 
 CREATE TABLE orders (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     client_id UUID REFERENCES clients(id),
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     confirmed_at TIMESTAMP,
     sended_at TIMESTAMP,
     delivered_at TIMESTAMP
