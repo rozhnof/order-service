225bba91a73f09c07ce1d8ec19d90eb7931b3b23
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2025-01-20 12:01:41 +0700

    refactor: remove unused order methods

--- internal/models/order.go
+++ internal/models/order.go
@@ -1,16 +1,9 @@
 package models
 
 import (
-	"errors"
 	"time"
 
 	"github.com/google/uuid"
-)
-
-const (
-	Confirmed = iota
-	Sended
-	Delivered
 )
 
 type Order struct {
@@ -28,53 +21,4 @@ func NewOrder(clientEmail string) Order {
 		ClientEmail: clientEmail,
 		CreatedAt:   time.Now(),
 	}
-}
-
-func (o *Order) SetStatus(status int) error {
-	if status == Confirmed {
-		return o.Confirm()
-	}
-
-	if status == Sended {
-		return o.Send()
-	}
-
-	if status == Delivered {
-		return o.Deliver()
-	}
-
-	return errors.New("invalid status")
-}
-
-func (o *Order) Confirm() error {
-	if o.DeliveredAt != nil {
-		return errors.New("confirm error: order already delivered")
-	}
-
-	if o.SendedAt != nil {
-		return errors.New("confirm error: order already sended")
-	}
-
-	now := time.Now()
-	o.ConfirmedAt = &now
-
-	return nil
-}
-
-func (o *Order) Send() error {
-	if o.DeliveredAt != nil {
-		return errors.New("confirm error: order already delivered")
-	}
-
-	now := time.Now()
-	o.SendedAt = &now
-
-	return nil
-}
-
-func (o *Order) Deliver() error {
-	now := time.Now()
-	o.DeliveredAt = &now
-
-	return nil
 }
